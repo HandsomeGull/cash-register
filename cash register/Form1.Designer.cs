26b1d780132ce67eac4a306abfdf910748264ea7
michbeau972 <michbeau972@SDS-D-114WH2>
2023-12-15 10:24:31 -0500

    Add project files.

---
+++ cash register/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace cash_register
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.slices = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Mons = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cash = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.getpaper = new System.Windows.Forms.Button();
+            this.cashback = new System.Windows.Forms.Label();
+            this.suboutput = new System.Windows.Forms.Label();
+            this.taxoutput = new System.Windows.Forms.Label();
+            this.totaloutput = new System.Windows.Forms.Label();
+            this.paper = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // slices
+            // 
+            this.slices.AutoSize = true;
+            this.slices.Location = new System.Drawing.Point(12, 27);
+            this.slices.Name = "slices";
+            this.slices.Size = new System.Drawing.Size(85, 13);
+            this.slices.TabIndex = 0;
+            this.slices.Text = "Number of slices";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nuber of drinks";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number of ghle pies";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "0";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Mons
+            // 
+            this.Mons.Location = new System.Drawing.Point(124, 321);
+            this.Mons.Name = "Mons";
+            this.Mons.Size = new System.Drawing.Size(100, 20);
+            this.Mons.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(124, 74);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "0";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(124, 129);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "sub total";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "tax";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Total";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(12, 285);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(212, 12);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 328);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Tendered";
+            // 
+            // cash
+            // 
+            this.cash.AutoSize = true;
+            this.cash.Location = new System.Drawing.Point(12, 389);
+            this.cash.Name = "cash";
+            this.cash.Size = new System.Drawing.Size(57, 13);
+            this.cash.TabIndex = 13;
+            this.cash.Text = "cash back";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 353);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(209, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // getpaper
+            // 
+            this.getpaper.Location = new System.Drawing.Point(15, 415);
+            this.getpaper.Name = "getpaper";
+            this.getpaper.Size = new System.Drawing.Size(209, 23);
+            this.getpaper.TabIndex = 15;
+            this.getpaper.Text = "button3";
+            this.getpaper.UseVisualStyleBackColor = true;
+            this.getpaper.Click += new System.EventHandler(this.getpaper_Click);
+            // 
+            // cashback
+            // 
+            this.cashback.AutoSize = true;
+            this.cashback.Location = new System.Drawing.Point(189, 389);
+            this.cashback.Name = "cashback";
+            this.cashback.Size = new System.Drawing.Size(35, 13);
+            this.cashback.TabIndex = 16;
+            this.cashback.Text = "label9";
+            // 
+            // suboutput
+            // 
+            this.suboutput.AutoSize = true;
+            this.suboutput.Location = new System.Drawing.Point(121, 191);
+            this.suboutput.Name = "suboutput";
+            this.suboutput.Size = new System.Drawing.Size(28, 13);
+            this.suboutput.TabIndex = 18;
+            this.suboutput.Text = "-------";
+            // 
+            // taxoutput
+            // 
+            this.taxoutput.AutoSize = true;
+            this.taxoutput.Location = new System.Drawing.Point(121, 223);
+            this.taxoutput.Name = "taxoutput";
+            this.taxoutput.Size = new System.Drawing.Size(28, 13);
+            this.taxoutput.TabIndex = 19;
+            this.taxoutput.Text = "-------";
+            this.taxoutput.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // totaloutput
+            // 
+            this.totaloutput.AutoSize = true;
+            this.totaloutput.Location = new System.Drawing.Point(121, 261);
+            this.totaloutput.Name = "totaloutput";
+            this.totaloutput.Size = new System.Drawing.Size(28, 13);
+            this.totaloutput.TabIndex = 20;
+            this.totaloutput.Text = "-------";
+            // 
+            // paper
+            // 
+            this.paper.BackColor = System.Drawing.Color.Cornsilk;
+            this.paper.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.paper.Location = new System.Drawing.Point(245, 9);
+            this.paper.Name = "paper";
+            this.paper.Size = new System.Drawing.Size(193, 429);
+            this.paper.TabIndex = 21;
+            this.paper.Text = "label8";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(450, 450);
+            this.Controls.Add(this.paper);
+            this.Controls.Add(this.totaloutput);
+            this.Controls.Add(this.taxoutput);
+            this.Controls.Add(this.suboutput);
+            this.Controls.Add(this.cashback);
+            this.Controls.Add(this.getpaper);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.cash);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Mons);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.slices);
+            this.Name = "Form1";
+            this.Text = "cash reg";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label slices;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Mons;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label cash;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button getpaper;
+        private System.Windows.Forms.Label cashback;
+        private System.Windows.Forms.Label suboutput;
+        private System.Windows.Forms.Label taxoutput;
+        private System.Windows.Forms.Label totaloutput;
+        private System.Windows.Forms.Label paper;
+    }
+}
+
---
+++ cash register/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cash_register
+{
+    public partial class Form1 : Form
+    {
+        double pizzaprice = 15;
+        double drinkprice = 2.50;
+        double pieprice = 3;
+        double eons = 0;
+        double Cashback = 0;
+
+        double pizza, drink, pie;
+
+        double subtotal;
+        double tax;
+        double pricetotal;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //1
+            //input 
+            pizza = Convert.ToDouble(textBox1.Text);
+            drink = Convert.ToDouble(textBox3.Text);
+            pie = Convert.ToDouble(textBox4.Text);
+
+            //math stuff
+            subtotal = pizzaprice * pizza + drinkprice * drink + pieprice * pie;
+            tax = subtotal * .13;
+            pricetotal = tax + subtotal;
+
+            suboutput.Text = $"{subtotal}";
+            taxoutput.Text = $"{tax}";
+            totaloutput.Text = $"{pricetotal.ToString("C")}";
+
+            //amount of mons given
+            
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            eons = Convert.ToDouble(Mons.Text);
+
+
+            subtotal = pizzaprice * pizza + drinkprice * drink + pieprice * pie;
+            tax = subtotal * .13;
+            pricetotal = tax + subtotal;
+
+            suboutput.Text = $"{subtotal}";
+            taxoutput.Text = $"{tax}";
+            totaloutput.Text = $"{pricetotal.ToString("C")}";
+           
+            Cashback = eons - pricetotal;
+
+            cashback.Text = $"{Cashback.ToString("C")}";
+        }
+
+        private void getpaper_Click(object sender, EventArgs e)
+        {
+            paper.Text = "                      Its Pizza Time \n";
+            paper.Text += "==============================\n\n";
+            paper.Text += $"        pizza's         x{pizza}  @  ${pizzaprice} \n";
+            paper.Text += $"        drink's         x{drink}  @  ${drinkprice} \n";
+            paper.Text += $"        pie's            x{pie}  @  ${pieprice}\n\n";
+            paper.Text += "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n\n";
+            paper.Text += $"        Subtotal                 ${subtotal} \n";
+            paper.Text += $"        tax                          ${tax} \n";
+            paper.Text += $"        total                        ${pricetotal}\n\n";
+            paper.Text += "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n\n";
+            paper.Text += $"        trendered                    ${eons}\n";
+            paper.Text += $"        Change                       ${Cashback}\n\n";
+            paper.Text += "==============================";
+
+
+            // paper.Text +=
+            /*
+            paper.Text = $" " +
+                $"                   Its Pizza Time" +
+                $"      order number ?" +
+                $"      date" +
+                $"      " +
+
+
+                $"      pizza's # @ #" +
+                $"      drink's # @ #" +
+                $"      pie's # @ #" +
+                $"    " +
+
+
+                $"      subtotal #" +
+                $"      tax #" +
+                $"      total #" +
+                $"" +
+
+
+                $"      tendered #" +
+                $"      change #" +
+                $"" +
+                                                                                
+
+
+                                                            
+
+                $"                  Have a nice day :D";
+
+            */
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
